33e66ddcc0e69e326f704025096757bccdba5c6d
Swathinipari <swathinipari@gmail.com>
2026-03-11 02:01:55 +0400

    Add files via upload

---
+++ day1/FirstProgram.java
@@ -0,0 +1,9 @@
+package org.testleaf.week1.day1;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("This is my First program");
+        
+    }
+
+}
