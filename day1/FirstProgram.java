eefcf5a783c62fb9cec3eb6be1b22baacaf05686
Swathinipari <swathinipari@gmail.com>
2026-03-11 02:02:48 +0400

    Delete day1 directory

--- day1/FirstProgram.java
+++
@@ -1,9 +0,0 @@
-package org.testleaf.week1.day1;
-
-public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("This is my First program");
-        
-    }
-
-}
